4af8fe7b9f2441d99066676ded2f629b43fdbcd9
Kamila Ryhan <kamila.ryhan@gmail.om>
2024-05-14 09:27:12 +0200

    ddd

---
+++ test2.py
@@ -0,0 +1,27 @@
+import random
+pole = []
+print(random.randint(1,16))
+
+# Vypis random delky
+for t in range(1,9):
+   pole.append(random.randint(1,13))
+print(pole)
+delka = input("Jaka je delka pole? ")
+if delka == "9":
+   print("Ano")
+else:
+   print("Ne")
+
+
+def nahodni_cisla(pole):
+   for k in range(pole(5)):
+      print(k)
+
+hra = int(input("Zadejte randomni delku: "))
+if hra == random.randint:
+   print("Spravně")
+else:
+   print("Špatně")
+
+
+
